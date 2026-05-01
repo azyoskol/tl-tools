b22cd5a7c614773f9b0333e34bc6ec3f9b155c7e
Andrey Zubarev <andrey@zubarev.net>
2026-05-01 08:04:06 +0300

    fix: correct import path in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,7 +4,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/metraly/cmd/api/handlers"
+	"github.com/getmetraly/metraly/cmd/api/handlers"
 )
 func main() {
 	r := chi.NewRouter()
